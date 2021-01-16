a2cee299fa49cb40b7e183fe68d01818ddcc67de
ZaglubotskiyMN <zaglubotskii@gmail.com>
2021-01-16 23:16:21 +0400

    + count (Zadanie 2)

---
+++ src/part1/lessons8/task2/schetchik.java
@@ -0,0 +1,44 @@
+package part1.lessons8.task2;
+
+public class schetchik {
+
+
+
+    public static void main(String[] args){
+        kollichestvo a1= new kollichestvo();
+        kollichestvo a2= new kollichestvo();
+        kollichestvo a3= new kollichestvo();
+        System.out.println("Колличество " +kollichestvo.);
+
+
+
+
+    }
+    public static class kollichestvo{
+     int kollichestvo;
+
+     int kollichestvo(int kollichestvo){
+         this.kollichestvo= kollichestvo;
+         kollichestvo++;
+         return kollichestvo;
+     }
+
+        public kollichestvo() {
+
+        }
+    }
+   /* public static class count{
+        int count;
+        count(){
+            count++;
+        }
+        */
+
+
+    }
+
+
+
+}
+
+
