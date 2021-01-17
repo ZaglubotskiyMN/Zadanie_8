f4f28d6a01137252753e033f480c8e4736fd30c0
ZaglubotskiyMN <zaglubotskii@gmail.com>
2021-01-17 11:30:37 +0400

    + count (Zadanie 2)_ispravleno

--- src/part1/lessons8/task2/schetchik.java
+++ src/part1/lessons8/task2/schetchik.java
@@ -1,44 +1,24 @@
-package part1.lessons8.task2;
-
 public class schetchik {
 
+    public static void main(String[] args) {
+        B someObject = new B();
+        System.out.println("Создано объектов: " + B.count);
 
+        B anotherObject = new B();
+        System.out.println("Создано объектов: " + B.count);
 
-    public static void main(String[] args){
-        kollichestvo a1= new kollichestvo();
-        kollichestvo a2= new kollichestvo();
-        kollichestvo a3= new kollichestvo();
-        System.out.println("Колличество " +kollichestvo.);
-
-
-
-
-    }
-    public static class kollichestvo{
-     int kollichestvo;
-
-     int kollichestvo(int kollichestvo){
-         this.kollichestvo= kollichestvo;
-         kollichestvo++;
-         return kollichestvo;
-     }
-
-        public kollichestvo() {
-
-        }
-    }
-   /* public static class count{
-        int count;
-        count(){
-            count++;
-        }
-        */
-
+        B theeObject = new B();
+        System.out.println("Создано объектов: "+ B.count);
 
     }
-
-
 
 }
 
+class B {
 
+    static int count;
+
+    B() {
+        count++;
+    }
+}
